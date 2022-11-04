5d880dcd3e1504a0cd18286d56c7c8cf476cef94
thaismoreiradev <contatothaismoreira@hotmail.com>
2022-11-03 21:26:18 -0300

    javascript tests

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ let breakTitle = document.querySelector("#break");
 let activeTitle = ("font-weight: bolder")
 let inactiveTitle = ("font-weight: normal")
 
+// for change the icon between play and pause
 let playing = false;
 
+// for change the session between work and break
 let working = true;
-
 
 let timer;
 
+let workingMinutes = true;
 
 let minutes = 24;
 let seconds = 59;
 
-let workMinutes;
-let breakMinutes;
 
 // display
 window.onload = () => {
@@ -28,8 +28,34 @@ window.onload = () => {
 
 
 
+const restartingTime = () => {
+    document.getElementById("restart").addEventListener("click", function(){
+        console.log("sei nãoooo")
+
+        if(working === true){
+            minutes = 27;
+
+            
+        }else{
+            minutes = 4;
+        }
+
+    })
+};
+
+
+
+
+
+
+
+
+
+
+
 
 // countdown
+
     let timerFunction = () => {
         // starting with work 25 minutes
         document.getElementById("minutes").innerHTML = minutes;
@@ -38,7 +64,7 @@ window.onload = () => {
         console.log(seconds)
 
         // starting here
-        
+        workingMinutes = true;
         seconds = seconds - 1;
         
         // when every work minute is over
@@ -52,23 +78,84 @@ window.onload = () => {
                 working = !working
 
                 if(working === false){
+                    workingMinutes = false
                     // changing the word in focus
                     workTitle.style = inactiveTitle;
                     breakTitle.style = activeTitle;
                     // starting break for 5 minutes
-                    minutes = 4;
-
+                    minutes = 4;                    
                 }else{
+                    
                     // changing the word in focus
                     workTitle.style = activeTitle;
                     breakTitle.style = inactiveTitle;
-                    // starting break for 5 minutes
+                    // starting work for 25 minutes
                     minutes = 24;
                 }
-
-            }
         }
     }
+
+    restartingTime();
+}
+
+
+
+
+
+
+//     let timerFunction = () => {
+//         // starting with work 25 minutes
+//         document.getElementById("minutes").innerHTML = workMinutes;
+//         document.getElementById("seconds").innerHTML = seconds;    
+
+//         console.log(seconds)
+
+//         // starting here
+        
+//         seconds = seconds - 1;
+        
+//         // when every work minute is over
+//         if(seconds === 0){
+            
+//             workMinutes = workMinutes - 1;
+//             seconds = 59;
+
+//             // when the 25 minutes ends
+//             if(workMinutes === -1){
+//                 working = false
+
+//                 if(working === false){
+//                     // changing the word in focus
+//                     workTitle.style = inactiveTitle;
+//                     breakTitle.style = activeTitle;
+//                     // starting break for 5 minutes
+//                     workMinutes = breakMinutes;
+
+
+//                     if(breakMinutes === -1){
+//                     working = true
+//                     // changing the word in focus
+//                     workTitle.style = activeTitle;
+//                     breakTitle.style = inactiveTitle;
+//                     // starting work for 25 minutes
+//                     breakMinutes = workMinutes;
+//                 }
+//             } 
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // starting timer
@@ -84,7 +171,7 @@ const startingTimer = () => {
         // minutes = minutes -1
 
         // starting countdown 1 sec
-        timer = setInterval(timerFunction, 10); 
+        timer = setInterval(timerFunction, 5); 
     }else{
 
         playing = false;
